1e33dcee77a741f62185cde8fe5d69437653e702
jungtaek kim <kgajungtaek@gmail.com>
2023-02-16 17:06:02 +0900

    docs : Transform Parent
    트랜스폼 컴포넌트의 부모-자식 상태

--- Assets/Scripts/Unity/UnityTransform.cs
+++ Assets/Scripts/Unity/UnityTransform.cs
@@ -86,4 +86,30 @@ public class UnityTransform : MonoBehaviour
 		// Euler각도를 Quaternion으로 변환
 		transform.rotation = Quaternion.Euler(0, 90, 0);
 	}
+
+	// <트랜스폼 부모-자식 상태>
+	// 트랜스폼은 부모 트랜스폼을 가질 수 있음
+	// 부모 트랜스폼이 있는 경우 부모 트랜스폼의 위치, 회전, 크기 변경이 같이 적용됨
+	// 이를 이용하여 계층적 구조를 정의하는데 유용함 (ex. 팔이 움직이면, 손가락도 같이 움직임)
+	// 하이어라키 창 상에서 드래그 & 드롭을 통해 부모-자식 상태를 변경할 수 있음
+	private void TransformParent()
+	{
+		GameObject newGameObject = new GameObject() { name = "NewGameObject" };
+
+		// 부모 지정
+		transform.parent = newGameObject.transform;
+
+		// 부모를 기준으로한 트랜스폼
+		// transform.localPosition	: 부모트랜스폼이 있는 경우 부모를 기준으로 한 위치
+		// transform.localRotation	: 부모트랜스폼이 있는 경우 부모를 기준으로 한 회전
+		// transform.localScale		: 부모트랜스폼이 있는 경우 부모를 기준으로 한 크기
+
+		// 부모 해제
+		transform.parent = null;
+
+		// 월드를 기준으로한 트랜스폼
+		// transform.localPosition == transform.position	: 부모트랜스폼이 없는 경우 월드를 기준으로 한 위치
+		// transform.localRotation == transform.rotation	: 부모트랜스폼이 없는 경우 월드를 기준으로 한 회전
+		// transform.localScale								: 부모트랜스폼이 없는 경우 월드를 기준으로 한 크기
+	}
 }
