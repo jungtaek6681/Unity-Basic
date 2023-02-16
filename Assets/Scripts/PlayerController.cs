ed4aa8084863e7e7d34f5b2ffbdb952f2ce1a013
jungtaek kim <kgajungtaek@gmail.com>
2023-02-16 16:43:44 +0900

    docs : Transform Translate & Rotate
    트랜스폼 컴포넌트의 이동과 회전

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,23 +7,31 @@ public class PlayerController : MonoBehaviour
 {
 	[SerializeField]
 	private float moveSpeed;
+	[SerializeField]
+	private float rotateSpeed;
 
-	private Vector3 moveDir;
+	private Vector3 inputDir;
 
 	private void Update()
 	{
 		UpdateMove();
+		UpdateRotate();
 	}
 
 	private void UpdateMove()
 	{
-		transform.position += moveDir * moveSpeed * Time.deltaTime;
+		transform.Translate(Vector3.forward * inputDir.z * moveSpeed * Time.deltaTime, Space.Self);
+	}
+
+	private void UpdateRotate()
+	{
+		transform.Rotate(Vector3.up, inputDir.x * rotateSpeed * Time.deltaTime, Space.World);
 	}
 
 	private void OnMove(InputValue value)
 	{
-		moveDir.x = value.Get<Vector2>().x;
-		moveDir.z = value.Get<Vector2>().y;
-		Debug.Log(moveDir);
+		inputDir.x = value.Get<Vector2>().x;
+		inputDir.z = value.Get<Vector2>().y;
+		Debug.Log(inputDir);
 	}
 }
