5664f89e215b91b9c03c11eb371d238dcf4bb14e
jungtaek kim <kgajungtaek@gmail.com>
2023-02-16 14:25:29 +0900

    docs : Input System
    유니티의 입력방식과 입력을 이용한 이동 구현

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField]
+	private float moveSpeed;
+
+	private Vector3 moveDir;
+
+	private void Update()
+	{
+		UpdateMove();
+	}
+
+	private void UpdateMove()
+	{
+		transform.position += moveDir * moveSpeed;
+	}
+
+	private void OnMove(InputValue value)
+	{
+		moveDir.x = value.Get<Vector2>().x;
+		moveDir.z = value.Get<Vector2>().y;
+		Debug.Log(moveDir);
+	}
+}
